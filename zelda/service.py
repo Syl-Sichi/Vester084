5bc1401f7cc2c1cd154c498f64f5eddd7f70b2c8
Sylvester Oguzie <48793004+Syl-Sichi@users.noreply.github.com>
2026-08-09 09:52:42 +0100

    Add long running Z.E.L.D.A. service

---
+++ zelda/service.py
@@ -0,0 +1,30 @@
+import logging
+import signal
+import threading
+
+from zelda.events.bus import Event, EventBus
+
+
+class ZeldaService:
+    """Long running host service with graceful shutdown and event dispatch."""
+
+    def __init__(self, event_bus: EventBus | None = None) -> None:
+        self.event_bus = event_bus or EventBus()
+        self.stop_event = threading.Event()
+        self.logger = logging.getLogger("zelda.service")
+
+    def request_stop(self, *_args) -> None:
+        self.logger.info("Shutdown requested")
+        self.stop_event.set()
+
+    def run(self) -> None:
+        signal.signal(signal.SIGINT, self.request_stop)
+        signal.signal(signal.SIGTERM, self.request_stop)
+        self.logger.info("Z.E.L.D.A. service started")
+        self.event_bus.publish(Event("service.started"))
+        try:
+            while not self.stop_event.wait(1.0):
+                pass
+        finally:
+            self.event_bus.publish(Event("service.stopped"))
+            self.logger.info("Z.E.L.D.A. service stopped")
